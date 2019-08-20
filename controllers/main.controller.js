81be12f1e3a330c0a14f40261a5d7db06b6a5248
traian-mihali <traianmihali696@gmail.com>
2019-08-20 21:05:20 +0300

    add custom elements

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,35 +1,67 @@
 (function() {
   var app = angular.module("githubViewer");
 
+  app.directive("validator", () => {
+    return {
+      restrict: "E",
+      template: `
+      <div class="alert alert-danger" ng-show="isTouched && !username">
+        Input field is empty
+      </div>
+      `
+    };
+  });
+
+  app.directive("search", () => {
+    return {
+      restrict: "E",
+      template: `
+      <input 
+        required 
+        type="search" 
+        id="search" 
+        ng-model="username" 
+        class="form-control" 
+        placeholder="Search a GitHub User..." 
+        ng-click="touched()"/>`
+    };
+  });
+
+  app.directive("btn", () => {
+    return {
+      restrict: "E",
+      template: `
+        <input 
+          type="submit" 
+          ng-click="search(username)" 
+          value="Search" 
+          class="btn btn-primary" 
+          ng-disabled="!username"
+        />`
+    };
+  });
+
+  app.directive("main", () => {
+    return {
+      restrict: "E",
+      template: `
+        <form name="searchUser" class="form-group" ng-submit="search(username)">
+          <search></search>
+          <validator></validator>
+        </form>
+        <btn></btn>
+      `
+    };
+  });
+
   var MainController = function($scope, $interval, $location) {
-    // var decrementCountdown = function() {
-    //   $scope.countdown -= 1;
-    //   if ($scope.countdown < 1) {
-    //     $scope.search($scope.username);
-    //   }
-    // };
-
-    // var countdownInterval = null;
-    // var startCountdown = function() {
-    //   countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
-    // };
-
     $scope.search = function(username) {
-      // if (countdownInterval) {
-      //   $interval.cancel(countdownInterval);
-      //   $scope.countdown = null;
-      // }
-
       $location.path("/user/" + username);
     };
 
     $scope.touched = function() {
       $scope.isTouched = true;
     };
-
-    // $scope.username = "traian-mihali";
-    // $scope.countdown = 10;
-    // startCountdown();
   };
 
   app.controller("controllers/main.controller", MainController);
